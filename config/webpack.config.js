d0da2d97a20454f2005fdcb63e1405815ed40a00
Nathan Nieuwenhuizen <18964@ma-web.nl>
2018-03-05 22:13:29 +0100

    sass url fix and sass minify added

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -19,8 +19,28 @@ module.exports = {
       },
             {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-        }
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                // If you are having trouble with urls not resolving add this setting.
+                                // See https://github.com/webpack-contrib/css-loader#url
+                                url: false,
+                                minimize: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }
     ]
     },
     resolve: {
