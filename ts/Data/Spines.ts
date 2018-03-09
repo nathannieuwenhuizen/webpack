15817318801af7c75876ed8ec8a9c17edaf22e05
nathannieuwenhuizen <18964@ma-web.nl>
2018-03-09 20:31:18 +0100

    small tweak

---
+++ ts/Data/Spines.ts
@@ -0,0 +1,11 @@
+export default class Spines 
+{
+
+    public static Chips: string = 'chips';
+
+    public static list: string[] = [
+        //Add spines to load
+        Spines.Chips
+    ];
+ 
+}
