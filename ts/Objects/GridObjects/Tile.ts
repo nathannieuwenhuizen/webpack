bbf31d2308de8bce412a3a0dfb39483601fdc068
nathannieuwenhuizen <18964@ma-web.nl>
2018-03-12 14:36:45 +0100

    adding random color and symbols to tiles

--- ts/Objects/GridObjects/Tile.ts
+++ ts/Objects/GridObjects/Tile.ts
@@ -3,7 +3,7 @@ import GridObject from './GridObject';
 import Atlases from '../../Data/Atlases';
 
 export enum icons {
-    stripe = 'stripe',
+    triangle = 'triangle',
     circle = 'circle',
     square = 'square'
 }
@@ -19,23 +19,69 @@ export default class Tile extends GridObject
     private color: colors;
     private icon: icons;
     private iconSprite: Phaser.Sprite;
-    constructor(game: Phaser.Game, xPos: number, yPos: number, icon: icons, color: colors) 
+    constructor(game: Phaser.Game, xPos: number, yPos: number)
     {
-        super(game, xPos, yPos, 'ui_ingame_icon_grey');
+        super(game, xPos, yPos, 'ui_ingame_icon_backdrop');
 
-        this.color = color;
-        this.icon = icon;
+        this.assignVallues();
 
-        this.iconSprite = new Phaser.Sprite(game, 0, 0, Atlases.Interface, '');
+        console.log(this.color, this.icon);
+        this.iconSprite = new Phaser.Sprite(game, 0, 0, Atlases.Interface, 'ui_ingame_icon_' + this.icon);
         this.iconSprite.anchor.set(.5);
+        switch (this.color) {
+            case colors.blue:
+            this.iconSprite.tint = 0x0000FF;
+            break;
+            case colors.red:
+            this.iconSprite.tint = 0x0000FF;
+            break;
+            case colors.green:
+            this.iconSprite.tint = 0x008000;
+            break;
+            default:
+            break;
+        }
         this.addChild(this.iconSprite);
     }
-    
-    public PopOut(): void {
+
+    public assignVallues(): void {
+        let random: number = Math.floor(Math.random() * 3);
+        switch (random){
+            case 0:
+            this.icon = icons.circle;
+            break;
+            case 1:
+            this.icon = icons.triangle;
+            break;
+            case 2:
+            this.icon = icons.square;
+            break;
+            default:
+            this.icon = icons.square;
+            break;
+        }
+        random = Math.floor(Math.random() * 3);
+        switch (random){
+            case 0:
+            this.color = colors.blue;
+            break;
+            case 1:
+            this.color = colors.red;
+            break;
+            case 2:
+            this.color = colors.green;
+            break;
+            default:
+            this.color = colors.blue;
+            break;
+        }
+    }
+
+    public popOut(): void {
         //
     }
-    
-    public AnimateInAway(): void {
+
+    public animateInAway(): void {
         //
     }
 }
